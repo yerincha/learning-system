25acb09cbe0735ab3803e1efb001708dc2bee02a
Yerin Cha <lenacha32@gmail.com>
2019-12-06 12:04:35 -0800

    Set up es-lint and fix all the grammatical errors

--- server/index.js
+++ server/index.js
@@ -15,12 +15,13 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '../public/dist')));
 
 app.use(require('./middleware/cookieParser'));
+
 app.use(Auth.createSession);
 
 // Load data from db
 
 app.get('/admin', (req, res) => {
-  db.Course.findOne()
+  db.Course.findAll()
     .then((result) => {
       res.send(result[0].dataValues);
     })
@@ -30,10 +31,10 @@ app.get('/admin', (req, res) => {
 });
 
 app.get('/student', (req, res) => {
-  db.StudentCourse.findOne({
+  db.StudentCourse.findAll({
     where: {
-      studentId: req.query.id
-    }
+      studentId: req.query.id,
+    },
   })
     .then((result) => {
       res.send(result);
@@ -47,11 +48,16 @@ app.get('/student', (req, res) => {
 // Sign up
 
 // Admin Sign up
+
 app.post('/api/admin_signup', (req, res) => {
+  // TODO: handle admin signup passcode
 
-  // TODO: handle admin signup passcode 
-
-  const { name, email, phone, password } = req.body;
+  const {
+    name,
+    email,
+    phone,
+    password,
+  } = req.body;
   const salt = utils.createRandom32String();
   const data = {
     name,
@@ -68,35 +74,42 @@ app.post('/api/admin_signup', (req, res) => {
       email,
     },
   })
-    .tap(user => {
-      console.log(user)
+    .tap((user) => {
+      // console.log(user);
       if (user.length !== 0) {
         throw user;
       }
     })
-    .then(user => {
-      console.log('creating')
+    .then(() => {
+      console.log('creating');
       return db.User.create(data);
     })
     .then((results) => {
       console.log(results);
-      var insertId = results.dataValues.id;
+      const insertId = results.dataValues.id;
       return db.Session.update({ userId: insertId }, { where: { id: req.session.id } });
     })
-    .then(user => {
+    .then(() => {
       console.log('New Admin signed up successfully');
       res.redirect('/');
     })
-    .catch(user => {
-      console.log('catch', user)
-      res.redirect('/admin_signup')
+    .catch((user) => {
+      console.log('catch', user);
+      res.redirect('/admin_signup');
     });
 });
 
 // Student sign up
-app.post('/api/signup', (req, res) => {
 
-  const { name, email, phone, password, cohort } = req.body;
+app.post('/api/signup', (req, res) => {
+  const {
+    name,
+    email,
+    phone,
+    password,
+    cohort,
+  } = req.body;
+
   const data = {
     name,
     email,
@@ -110,62 +123,60 @@ app.post('/api/signup', (req, res) => {
       email,
     },
   })
-    .tap(user => {
+    .tap((user) => {
       // console.log(user)
       if (user.length !== 0) {
         throw user;
       }
     })
-    .then(user => {
-      // console.log('creating')
+    .then(() => {
+      console.log('creating');
       return db.User.create(data);
     })
     .then((results) => {
       // console.log(results);
-      var insertId = results.dataValues.id;
+      const insertId = results.dataValues.id;
       return db.Session.update({ userId: insertId }, { where: { id: req.session.id } });
     })
-    .then(user => {
-      console.log('New student signed up successfully');
+    .then(() => {
+      // console.log('New student signed up successfully');
       res.redirect('/');
     })
-    .catch(user => {
-      console.log('catch', user)
-      res.redirect('/signup')
+    .catch((user) => {
+      console.log('catch', user);
+      res.redirect('/signup');
     });
 });
 
 // log in
 
-app.post('/api/login', (req, res, next) => {
+app.post('/api/login', (req, res) => {
   if (!req.body.email || !req.body.password) {
     // send back a 404 if input is not valid
     return res.sendStatus(404);
   }
   const { email, password } = req.body;
   return db.User.findOne({
-      where: {
-        email: email,
-      }
-    })
-    .then(user => {
+    where: {
+      email,
+    },
+  })
+    .then((user) => {
       if (!user) {
         throw user;
       }
       return user;
     })
-    .then(data => {
-      return utils.compareHash(password, data.password, data.salt);
-    })
-    .then(bool => {
+    .then((data) => utils.compareHash(password, data.password, data.salt))
+    .then((bool) => {
       if (!bool) throw bool;
-      console.log('Successfully logged in!')
+      console.log('Successfully logged in!');
     })
-    .catch(user => {
-      console.log('login error')
+    .catch(() => {
+      console.log('login error');
       // res.redirect('/login')
     });
-})
+});
 
 
 // listening port
